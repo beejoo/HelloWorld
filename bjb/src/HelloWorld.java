0f3f6bd6f645081b514e8c9aa53139fc82d36c9f
beejoo <beejoo99@gmail.com>
2012-11-16 00:56:47 +0900

    second

--- bjb/src/HelloWorld.java
+++ bjb/src/HelloWorld.java
@@ -2,4 +2,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("This is OK.");
 	}
+	
+	public void test() {
+		System.out.println("This is a test.");
+	}
 }
